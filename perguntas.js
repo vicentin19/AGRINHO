485a7ef2cde94a63cce2f6e8730015f1d959b697
vicentin19 <silva.vicentin@escola.pr.gov.br>
2025-06-13 10:56:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,108 @@
+criaCartao(
+    'Episódio I: A Ameaça Fantasma',
+    'Quem descobriu Anakin Skywalker em Tatooine? ',
+    'Qui-Gon Jinn, um Mestre Jedi, encontra Anakin em Tatooine e percebe seu forte potencial na Força.'
+)
+
+criaCartao(
+    'Episódio II: Ataque dos Clones',
+    ' Qual planeta é atacado no início do filme, levando Obi-Wan a investigar?',
+    ' O planeta Coruscant, onde a Senadora Padmé Amidala sofre uma tentativa de assassinato.'
+)
+
+criaCartao(
+    'Episódio III: A Vingança dos Sith',
+    'Quais Jedis sobrevivem à Ordem 66 e é visto no final do filme?',
+    'Yoda e Obi-Wan Kenobi sobrevivem, mas a maioria dos Jedi é morta.'
+)
+
+criaCartao(
+    'Episódio IV: Uma Nova Esperança',
+    'Qual é o nome do planeta destruído pela Estrela da Morte?',
+    'Alderaan, o planeta natal da Princesa Leia.'
+)
+
+criaCartao(
+    'Episódio V: O Império Contra-Ataca',
+    'Em qual planeta Luke treina com Yoda?',
+    'Dagobah, um planeta pantanoso e repleto de vida.'
+)
+
+criaCartao(
+    'Episódio VI: O Retorno de Jedi',
+    'Como o Imperador Palpatine morre?',
+    ' Darth Vader o arremessa no poço da Estrela da Morte II, sacrificando-se para salvar Luke.'
+)
+
+criaCartao(
+    'Episódio VII: O Despertar da Força',
+    'Quem é o pai de Rey?',
+    ' Ela é filha de um clone de Palpatine.'
+)
+
+criaCartao(
+    'Episódio VIII: Os Últimos Jedi',
+    'Qual é o nome do líder da Resistência que sacrifica seu navio para salvar os outros? ',
+    'Almirante Holdo, que realiza uma manobra de hiperespaço contra a frota da Primeira Ordem.'
+)
+
+criaCartao(
+    'Episódio IX: A Ascensão Skywalker',
+    'Como Rey demonstra sua ligação com a linhagem Skywalker no final?',
+    ' Ela se declara "Rey Skywalker", honrando a família que a acolheu.'
+)
+
+criaCartao(
+    'Episódio I: A Ameaça Fantasma',
+    'Qual é a espécie do vilão Darth Maul?',
+    'Darth Maul é um Zabrak, originário do planeta Iridonia, reconhecível por seus chifres e tatuagens vermelhas e pretas.'
+)
+
+criaCartao(
+    'Episódio II: Ataque dos Clones  ',
+    'Qual é o nome do exército de clones que os Jedi passam a comandar?',
+    ' O Grande Exército da República, composto por clones criados em Kamino.'
+)
+
+criaCartao(
+    'Episódio III: A Vingança dos Sith',
+    'Como o Chanceler Palpatine é revelado como Darth Sidious?',
+    'Ele revela sua verdadeira identidade ao manipular Anakin e, em seguida, enfrenta Mace Windu e outros Jedi.'
+)
+
+criaCartao(
+    'Episódio IV: Uma Nova Esperança',
+    'Quem é o piloto que destruiu a Estrela da Morte no final do filme?',
+    'Luke Skywalker, com a ajuda de Han Solo e da Força.'
+)
+
+criaCartao(
+    'Episódio V: O Império Contra-Ataca',
+    'Qual a revelação chocante que Darth Vader faz a Luke?',
+    ' "Eu sou seu pai.".'
+)
+
+criaCartao(
+    'Episódio VI: O Retorno de Jedi',
+    'Quem é Jabba, o Hutt?',
+    ' Um poderoso criminoso que mantém Han Solo congelado em carbonita e controla Tatooine.'
+)
+
+criaCartao(
+    'Episódio VII: O Despertar da Força',
+    'Qual é o nome da nova ameaça criada pela Primeira Ordem? ',
+    ' Starkiller Base, uma super arma capaz de destruir sistemas estelares inteiros'
+)
+
+criaCartao(
+    'Episódio VIII: Os Últimos Jedi',
+    'Como Luke Skywalker morre?',
+    'Ele projeta-se através da Força para enfrentar Kylo Ren e depois desaparece, tornando-se um com a Força.'
+)
+
+criaCartao(
+    'Episódio IX: A Ascensão Skywalker',
+    'Quem é revelado como o verdadeiro vilão por trás da Primeira Ordem?',
+    'Darth Sidious (Palpatine), que retorna em um corpo clonado. '
+)
+
